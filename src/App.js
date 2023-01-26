7562fc29d2da1d4da15249adaaa4ff1e415908e8
Usama <ut303397@gmail.com>
2023-01-26 15:32:32 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-
 import { CardList } from './components/card-list/card-list.component'
-
 import { SearchBox } from './components/search-box/search-box.component'
-
 import './App.css';
 
 class App extends Component{
@@ -12,31 +9,36 @@ class App extends Component{
     
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: ""
     }; 
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({monsters: users}));
+    .then(users => {
+      users.map(user => console.log(user.name));
+      this.setState({monsters: users});
+    });
   }
-  onSearchChange = event => {
+
+  handleChange = event => {
     this.setState({ searchField: event.target.value });
   };
-
+   
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase()))
-    
-    
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
       return (
        <div className="App">
         <h1>Monsters Rolodex</h1>
-        <SearchBox onSearchChange={this.onSearchChange}/>
+        <SearchBox 
+        handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
     </div>
-    )
+    );
   }
 }
 
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
 import './search-box.styles.css';
 
-export const SearchBox = ({ placeholder, handleChange }) => (
-    <input
-    className="search"
-    type='search' 
-        placeholder={placeholder} 
-        onChange={handleChange} 
+export const SearchBox = props => {
+    return (
+        <input
+        className="search"
+        type='search' 
+        placeholder={props.placeholder} 
+        onChange={props.handleChange} 
         />
-)
+    );
+};
